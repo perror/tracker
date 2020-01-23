97e1aece1e3ce4a06eeffb67cb92b119a4186192
mrglm <plet.aurelien@gmail.com>
2020-01-23 14:08:28 +0100

    Formatting and adding comments in trace.h

--- include/trace.h
+++ include/trace.h
@@ -13,20 +13,32 @@
 #ifndef _TRACE_H
 #define _TRACE_H
 
+#include <inttypes.h>
 #include <stdbool.h>
 #include <stdlib.h>
-#include <inttypes.h>
 
 #define DEFAULT_HASHTABLE_SIZE 65536 /* 2^16 */
 
 /* A more convenient byte_t type */
 typedef uint8_t byte_t;
 
-/* ***** Handling assembly instructions ***** */
 
+/* ***** Definitions of all the struct used ***** */
+
+/* ***** Handling assembly instructions ***** */
 typedef struct _instr_t instr_t;
 
+/* ***** Hashtables to store cfg nodes ***** */
+typedef struct _hashtable_t hashtable_t;
+
+/* ***** cfg nodes keeping track of the executions ***** */
 typedef struct _cfg_t cfg_t;
+
+/* ***** Linked list to store a trace ***** */
+typedef struct _trace_t trace_t;
+
+
+/* ***** instr_t functions ***** */
 
 /* Return a new instr_t struct, NULL otherwise (and set errno) */
 instr_t *instr_new (const uintptr_t addr,
@@ -45,9 +57,8 @@ size_t instr_get_size (instr_t * const instr);
 /* Get a pointer to the opcodes of the instruction */
 uint8_t * instr_get_opcodes (instr_t * const instr);
 
-/* ***** Hashtables to store instructions ***** */
 
-typedef struct _hashtable_t hashtable_t;
+/* ***** hashtable_t functions ***** */
 
 /* Return an hash index for the instruction */
 uint64_t hash_instr (const instr_t *instr);
@@ -70,16 +81,15 @@ size_t hashtable_entries (hashtable_t *ht);
 /* Count the number of collisions in the hashtable */
 size_t hashtable_collisions (hashtable_t *ht);
 
-/* ***** Linked list to store a trace ***** */
 
-typedef struct _trace_t trace_t;
+/* ***** trace_t functions ***** */
 
-/* Creates a trace and initialize the first element with hash_index
+/* Creates a trace and initialize it with ins
 Returns a pointer to the created trace, or NULL if an error occured */
 trace_t *trace_new (instr_t *ins);
 
-/* Insert an element initialized with hash_index and insert it after t
-Returns a pointer to the created element or NULL if an error occured*/
+/* Creates an element initialized with ins and insert it after t
+Returns a pointer to the created element or NULL if an error occured */
 trace_t *trace_insert (trace_t *t, instr_t *ins);
 
 /* Free every element in the trace t */
@@ -88,18 +98,22 @@ void trace_delete (trace_t *t);
 /* Returns a pointer to the first element where t2 differs from t1 */
 trace_t *trace_compare (trace_t *t1, trace_t *t2);
 
-/* ***** CFG ***** */
 
+/* ***** cfg_t functions ***** */
 
-
+/* Creates a cfg and it with hash_index
+Returns a pointer to the created trace, or NULL if an error occured */
 cfg_t *cfg_new (hashtable_t *ht, instr_t *ins);
 
+/* Auxiliary function for cfg_insert */
 cfg_t *aux_cfg_insert (cfg_t *CFG, cfg_t *new);
 
+/* Creates an element initialized with ins and insert it in CFG's succesors
+Returns a pointer to the created element or NULL if an error occured*/
 cfg_t *cfg_insert (hashtable_t *ht, cfg_t *CFG, instr_t *ins);
 
+/* Free every allocated field of CFG, as well as CFG itself */
 void cfg_delete (cfg_t *CFG);
-
 
 
 #endif /* _TRACE_H */
