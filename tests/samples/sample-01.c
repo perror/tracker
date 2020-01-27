f875fcfae4d78eaab22abcb9ce5df7c1a5cb7af1
Emmanuel Fleury <emmanuel.fleury@u-bordeaux.fr>
2020-01-27 15:08:32 +0100

    Restructuring Makefiles and starting to populate tests

---
+++ tests/samples/sample-01.c
@@ -0,0 +1,11 @@
+/*
+ * Shortest possible program in C
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+  puts ("Hello World!");
+  return 0;
+}
---
+++ tests/samples/sample-02.cc
@@ -0,0 +1,11 @@
+/*
+ * Shortest possible program in C++
+ */
+
+#include <iostream>
+
+int main(void)
+{
+  std::cout << "Hello World!" << std::endl;
+  return 0;
+}
