33d0fa8c005bc6342e4a4636a0ac85c3edd2a0ed
Emmanuel Fleury <emmanuel.fleury@u-bordeaux.fr>
2020-01-31 10:44:19 +0100

    Adding a first implementation for handling executable

---
+++ include/executable.h
@@ -0,0 +1,40 @@
+/*
+ * tracker is an hybrid trustworthy disassembler that tries to limit the number
+ * of false positive paths discovered.
+ *
+ *  Written and maintained by Emmanuel Fleury <emmanuel.fleury@u-bordeaux.fr>
+ *
+ * Copyright 2019 University of Bordeaux, CNRS (UMR 5800), France.
+ * All rights reserved.
+ *
+ * This software is released under a 3-clause BSD license (see COPYING file).
+ */
+
+#ifndef _EXECUTABLE_H
+#define _EXECUTABLE_H
+
+/* Executable type */
+typedef executable_t _executable_t;
+
+/* Platform architecture arch_t type */
+typedef enum { unknown_arch = 0, x86_32_arch, x86_64_arch } arch_t;
+
+/* Create and initialize a new executable_t */
+executable_t *exec_init (char *execfilename);
+
+/* Free the given executable */
+void exec_free (executable_t *exec);
+
+/* Get executable architecture */
+arch_t exec_arch (executable_t *exec);
+
+/* Iterator on executable sections, return NULL after last item and cycle */
+char *executable_section_next (executable_t *exec);
+
+/* Get the section that contains the given address */
+char *executable_get_section_by_addr (executable_t *exec, uintptr_t addr);
+
+/* Get symbol by address, return NULL if no symbol is matching */
+char *executable_get_symbol_by_addr (executable_t *exec, uintptr_t addr);
+
+#endif /* _EXECUTABLE_H */
---
+++ src/executable.c
@@ -0,0 +1,120 @@
+/*
+ * tracker is an hybrid trustworthy disassembler that tries to limit the number
+ * of false positive paths discovered.
+ *
+ *  Written and maintained by Emmanuel Fleury <emmanuel.fleury@u-bordeaux.fr>
+ *
+ * Copyright 2019 University of Bordeaux, CNRS (UMR 5800), France.
+ * All rights reserved.
+ *
+ * This software is released under a 3-clause BSD license (see COPYING file).
+ */
+
+#include "executable.h"
+
+#include <stdio.h>
+
+#include <elf.h>
+#include <err.h>
+
+#include <sys/stat.h>
+
+typedef struct
+{
+  arch_t arch;
+  union
+  {
+    Elf32_Ehdr elf32;
+    Elf64_Ehdr elf64;
+  } header;
+} _executable_t;
+
+executable_t *
+exec_init (char *execfilename)
+{
+  struct stat exec_stats;
+  if (stat (execfilename, &exec_stats) == -1)
+    err (EXIT_FAILURE, "error: '%s'", execfilename);
+
+  if (!S_ISREG (exec_stats.st_mode) || !(exec_stats.st_mode & S_IXUSR))
+    errx (EXIT_FAILURE, "error: '%s' is not an executable file", execfilename);
+
+  /* Check if given file is an executable and discover its architecture */
+  FILE *execfile = fopen (execfilename, "rb");
+  if (!execfile)
+    err (EXIT_FAILURE, "error: '%s'", execfilename);
+
+  /* Open file */
+  char buf[4] = {0};
+  if (fread (&buf, 4, 1, execfile) != 1)
+    errx (EXIT_FAILURE, "error: cannot read '%s'", execfilename);
+
+  /* Check ELF magic number (first 4 bytes: 0x7f "ELF") */
+  if (buf[0] != 0x7f || strncmp (&(buf[1]), "ELF", 3) != 0)
+    errx (EXIT_FAILURE, "error: '%s' is not an ELF binary", execfilename);
+
+  /* Extract executable architecture (byte at 0x12) */
+  fseek (execfile, 0x12, SEEK_SET);
+  if (fread (&buf, 1, 1, execfile) != 1)
+    errx (EXIT_FAILURE, "error: cannot read '%s'", execfilename);
+  rewind (execfile);
+
+  executable_t *exec = malloc (sizeof (executable_t));
+  if (exec == NULL)
+    return NULL;
+
+  switch (buf[0])
+    {
+    case 0x03:
+      exec_hdr.arch = x86_32_arch;
+      fread (&(exec_hdr.header.elf32), 1, sizeof (Elf32_Ehdr), execfile);
+      break;
+
+    case 0x3e:
+      exec_hdr.arch = x86_64_arch;
+      fread (&(exec_hdr.header.elf64), 1, sizeof (Elf64_Ehdr), execfile);
+      break;
+
+    default:
+      errx (EXIT_FAILURE, "error: '%s' unsupported architecture", execfilename);
+    }
+
+  /* Closing file after verifications */
+  fclose (execfile);
+
+  return exec_hdr;
+}
+
+void
+exec_free (executable_t *exec)
+{
+  free (exec);
+}
+
+/* Get executable architecture */
+arch_t
+exec_arch (executable_t *exec)
+{
+  if (exec == NULL)
+    return NULL;
+
+  return exec->arch;
+};
+
+char *
+executable_section_next (executable_t *exec)
+{
+  return NULL;
+}
+
+char *
+executable_get_section_by_addr (executable_t *exec, uintptr_t addr)
+{
+  return NULL;
+}
+
+char *
+executable_get_symbol_by_addr (executable_t *exec, uintptr_t addr)
+{
+  return NULL;
+}
